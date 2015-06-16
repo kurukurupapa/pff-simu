42794a7d42ba07ac61d246d34d7ab2f9e886d4e0
kurukurupapa <gootarapapa@gmail.com>
2015-06-16 21:14:28 +0900

    Initial commit.

---
+++ src/main/java/com/kurukurupapa/pffsimu/PffSimuApplication.java
@@ -0,0 +1,12 @@
+package com.kurukurupapa.pffsimu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PffSimuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PffSimuApplication.class, args);
+	}
+}
