8807f6aebe242b71e6b7f30921ae1eebdd8559a7
kurukurupapa <gootarapapa@gmail.com>
2015-08-06 14:03:18 +0900

    リファクタリング

--- src/test/java/com/kurukurupapa/pff/dp01/MemoTest.java
+++ src/test/java/com/kurukurupapa/pff/dp01/MemoTest.java
@@ -43,7 +43,7 @@ public class MemoTest extends BaseTestCase {
 		Party actual = sut.get(0, 0);
 
 		// 検証
-		assertThat(actual.toString(), is("null,ヴァニラ"));
+		assertThat(actual.toString(), is("0,ヴァニラ"));
 	}
 
 	@Test
@@ -65,7 +65,7 @@ public class MemoTest extends BaseTestCase {
 		Party actual = sut.get(0, 0);
 
 		// 検証
-		assertThat(actual.toString(), is("null,アーロン"));
+		assertThat(actual.toString(), is("0,アーロン"));
 	}
 
 }
